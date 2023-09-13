b2f32118b60fc2d410826666e8dafc9e354dd28d
Ivan Dimov <iidimov18@codingburgas.bg>
2023-09-13 16:15:14 +0300

    Add mysql database connection

---
+++ database.ts
@@ -0,0 +1,31 @@
+import mysql, { Connection } from 'mysql';
+
+export default class MySql{
+    private connection!: Connection;
+
+    private static pool: MySql;
+    
+
+    public static getConnection() {
+        if(!this.pool){
+            this.pool = new MySql();
+        }
+        return this.pool.connection;
+    }
+
+    private constructor() {
+        this.connection = mysql.createConnection({
+          host: process.env.DB_HOST,
+          user: process.env.DB_USER,
+          password: process.env.DB_PASS,
+          database: process.env.DB_DB,
+        });
+    
+        this.connect();
+      }
+    
+      connect() {
+        this.connection.connect();
+        
+      }
+}
--- index.ts
+++ index.ts
@@ -1,16 +1,24 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import MySql from './database'
 
+
+let mySql = MySql.getConnection()
 
 const app: Express = express();
 const port = process.env.PORT;
 
 
-app.get('/', (req: Request, res: Response) => {
 
+app.get('/', (req: Request, res: Response) => {
+   mySql.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+    if (error) throw error;
+    console.log('The solution is: ', results[0].solution);
+  });
   res.send('Express + TypeScript Server');
 });
+
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
